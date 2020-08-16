a22b30aea07784400172ec36c5fe6fbbd025907d
dmslw <dominik.mysliwy@gmail.com>
2020-08-16 22:42:43 +0200

    6.2 Create Unit Test for Class Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,39 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
 
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator(1, 2);
+
+        int result1 = calculator.addAToB();
+        int result2 = calculator.subtractAFromB();
+
+        if (result1 == 3){
+            System.out.println("test OK");
+        }
+        if (result2 == 3){
+            System.out.println("test drugiej metody OK");
+
+        } else {
+
+            System.out.println("error!");
+        }
+        
+            
+        }
     }
-}
+
